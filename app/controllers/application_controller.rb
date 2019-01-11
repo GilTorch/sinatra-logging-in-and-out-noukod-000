b4359a3a7718b6732792c70425c9cca212d9b755
Gilbert Torchon <gilbert@noukod.com>
2019-01-11 18:23:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < Sinatra::Base
     @user=User.find_by({username:@params[:username]})
     if !@user.nil?
       session[:id]=@user.id
-      redirect("/account")
+      redirect to("/account")
     else
       redirect("/login")
     end
